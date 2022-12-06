547fee157aea684c65aa320ec621677e5034a998
Uttej <uttej60@gmail.com>
2022-12-06 08:25:04 -0800

    Environment interaction loop

--- inverted-pendulum-ppo/main.py
+++ inverted-pendulum-ppo/main.py
@@ -1,6 +1,6 @@
 import gym
 
-env = gym.make('Pendulum-v1-custom')
+env = gym.make('Pendulum-v1')
 
 # sample hyperparameters
 batch_size = 10000
@@ -8,3 +8,12 @@ epochs = 30
 learning_rate = 1e-2
 hidden_size = 8
 n_layers = 2
+
+# Environment interaction loop
+obs = env.reset()
+
+TRAINING_SIZE = 100
+for _ in range(TRAINING_SIZE):
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    env.render()
