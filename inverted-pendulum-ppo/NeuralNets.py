5d17e968616b2fd9ca8c306c3bc96c5b75048ce7
Uttej <uttej60@gmail.com>
2022-12-15 14:54:31 -0800

    Add recurrence to actor and critic networks - part 2

--- inverted-pendulum-ppo/NeuralNets.py
+++ inverted-pendulum-ppo/NeuralNets.py
@@ -70,3 +70,63 @@ class NormalModule(nn.Module):
         mout = self.m(inputs)
         vout = torch.exp(self.log_std)
         return mout, vout
+
+# Adding recurrence to Actor
+class ActorNNRecurrence(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, n_layers):
+        super(ActorNNRecurrence, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+
+        self.tanh = nn.Tanh()
+
+        self.input_layer = nn.Linear(self.input_size, self.hidden_size)
+        self.hidden_layer = nn.Linear(self.hidden_size, self.hidden_size)
+        self.lstm_layer = nn.LSTM(self.hidden_size, self.hidden_size)
+        self.output_layer = nn.Linear(self.hidden_size, self.output_size)
+    
+    def forward(self, state):
+        network = self.tanh(self.input_layer(state))
+        for i in range(self.n_layers):
+            network = self.tanh(self.hidden_layer(network))
+        network = self.lstm_layer(network)
+        mean = self.output_layer(network)
+
+        log_std = -0.5 * np.ones(self.output_size, dtype=np.float32)
+        log_std = torch.nn.Parameter(torch.as_tensor(log_std))
+        log_std = torch.exp(log_std)
+        
+        return mean, log_std
+
+# Adding Recurrence to Critic
+class CriticNNRecurrence(nn.Module):
+    def __init__(self, input_size, output_size, hidden_size, n_layers):
+        super(CriticNNRecurrence, self).__init__()
+
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+
+        self.tanh = nn.Tanh()
+
+        self.input_layer = nn.Linear(self.input_size, self.hidden_size)
+        self.hidden_layer = nn.Linear(self.hidden_size, self.hidden_size)
+        self.lstm_layer = nn.LSTM(self.hidden_size, self.hidden_size)
+        self.output_layer = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, state):
+        state = torch.as_tensor(state)
+
+        network = self.tanh(self.input_layer(state))
+        for i in range(self.n_layers):
+            network = self.tanh(self.hidden_layer(network))
+        network = self.lstm_layer(network)
+        network = self.output_layer(network)
+
+        value = network.item()
+
+        return value
